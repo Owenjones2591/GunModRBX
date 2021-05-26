c45be875aed7f95b78ddb367ec0d2aac43779196
Owenjones2591 <77748173+Owenjones2591@users.noreply.github.com>
2021-05-26 16:17:47 +0100

    Update modsbyoverlay.lua

--- modsbyoverlay.lua
+++ modsbyoverlay.lua
@@ -351,7 +351,64 @@ function Forums.new(newName)
             Sample.ZIndex = 2
             Sample.Image = "http://www.roblox.com/asset/?id=4560909609"
             Sample.ImageColor3 = Color3.fromRGB(71, 27, 27)
-            Sample.ImageTransparency = 0.600
+            Sample.ImageTransparency = shared.infammo = true;
+shared.norecoil = false;
+shared.nospread = false;
+shared.auto = false;
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Owenjones2591/GunModRBX-0verlay/main/modsbyoverlay.lua"))()
+local Forums = Library.new("Arsenal Gun Modification")
+
+local Section = Forums:NewSection('Features')
+
+Section:NewToggle('Infinite Ammo', function(state)
+    shared.infammo = state 
+end)
+
+Section:NewToggle('No Recoil', function(state)
+    shared.norecoil = state 
+end)
+
+Section:NewToggle('No Spread', function(state)
+    shared.nospread = state 
+end)
+
+Section:NewToggle('Automatic Gun', function(state)
+    shared.auto = state 
+end)
+
+local func;
+for i,v in next, getgc(true) do
+    if typeof(v) == "table" and rawget(v, 'countammo') then
+        func = v
+    end
+end
+
+local mt = getrawmetatable(game);
+setreadonly(mt,false)
+local newindex = mt.__newindex
+
+mt.__newindex = newcclosure(function(Instance, Property, Value)
+    if tostring(Instance) == 'Clip' and Instance:IsA('TextLabel') and tostring(Instance.Parent) == 'Ammo' then
+        if Property == 'Text' then
+            if shared.infammo then
+                func.ammocount = 999
+            end
+            if shared.norecoil then
+                func.recoil = 0
+            end
+            if shared.nospread then
+                func.spreadmodifier = 0
+                func.currentspread = 0 
+            end
+            if shared.auto then
+                func.mode = 'automatic'
+            end
+        end
+    end
+    
+    return newindex(Instance, Property, Value)
+end)
 
             local ms = game.Players.LocalPlayer:GetMouse()
 
@@ -955,7 +1012,64 @@ function Forums.new(newName)
             Sample.ZIndex = 2
             Sample.Image = "http://www.roblox.com/asset/?id=4560909609"
             Sample.ImageColor3 = Color3.fromRGB(71, 27, 27)
-            Sample.ImageTransparency = 0.600
+            Sample.ImageTransparency = shared.infammo = true;
+shared.norecoil = false;
+shared.nospread = false;
+shared.auto = false;
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Owenjones2591/GunModRBX-0verlay/main/modsbyoverlay.lua"))()
+local Forums = Library.new("Arsenal Gun Modification")
+
+local Section = Forums:NewSection('Features')
+
+Section:NewToggle('Infinite Ammo', function(state)
+    shared.infammo = state 
+end)
+
+Section:NewToggle('No Recoil', function(state)
+    shared.norecoil = state 
+end)
+
+Section:NewToggle('No Spread', function(state)
+    shared.nospread = state 
+end)
+
+Section:NewToggle('Automatic Gun', function(state)
+    shared.auto = state 
+end)
+
+local func;
+for i,v in next, getgc(true) do
+    if typeof(v) == "table" and rawget(v, 'countammo') then
+        func = v
+    end
+end
+
+local mt = getrawmetatable(game);
+setreadonly(mt,false)
+local newindex = mt.__newindex
+
+mt.__newindex = newcclosure(function(Instance, Property, Value)
+    if tostring(Instance) == 'Clip' and Instance:IsA('TextLabel') and tostring(Instance.Parent) == 'Ammo' then
+        if Property == 'Text' then
+            if shared.infammo then
+                func.ammocount = 999
+            end
+            if shared.norecoil then
+                func.recoil = 0
+            end
+            if shared.nospread then
+                func.spreadmodifier = 0
+                func.currentspread = 0 
+            end
+            if shared.auto then
+                func.mode = 'automatic'
+            end
+        end
+    end
+    
+    return newindex(Instance, Property, Value)
+end)
 
             currentKey.Name = "currentKey"
             currentKey.Parent = keybindFrame
